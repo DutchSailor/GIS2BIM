296f665ae1f8b2f4473eb4747a579af85da99f74
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2020-12-24 22:50:39 +0100

    FreeCAD update

--- GIS2BIM_FreeCAD.py
+++ GIS2BIM_FreeCAD.py
@@ -1,21 +1,41 @@
 ## GIS2BIM Library
 
-a = GIS2BIM_GetLocationDataNetherlands("dordrecht","Lange%20Geldersekade","2")
+import GIS2BIM_Lib
+import urllib.request
+import xml.etree.ElementTree as ET
+import json
+
+a = GIS2BIM_Lib.GIS2BIM_GetLocationDataNetherlands("dordrecht","Lange%20Geldersekade","2")
 width = 500
 Rdx = float(a[0])
 Rdy = float(a[1])
 
 Rdx = 104857.637
 Rdy = 425331.936
-Bbox = GIS2BIM_CreateBoundingBox(Rdx,Rdy,width,width,2)
+Bbox = GIS2BIM_Lib.GIS2BIM_CreateBoundingBox(Rdx,Rdy,width,width,2)
 
-curvesCadastralParcels = GIS2BIM_PointsFromWFS(DutchGEOCadastreServerRequest1,Bbox,xPathCadastre1,-Rdx,-Rdy,1000,1)
-curvesBuildings = GIS2BIM_PointsFromWFS(DutchGEOBAG,Bbox,xPathCadastre1,-Rdx,-Rdy,1000,1)
-curvesRuimtelijkePlannen = GIS2BIM_PointsFromWFS(DutchGEORuimtelijkeplannenBouwvlakServerRequest,Bbox,xPathRuimtelijkePlannen,-Rdx,-Rdy,1000,1)
-textDataCadastralParcels = GIS2BIM_DataFromWFS(DutchGEOCadastreServerRequest2,Bbox,xPathCadastre2,xPathStringsCadastreTextAngle,-Rdx,-Rdy,1000,1)
-textDataOpenbareRuimtenaam = GIS2BIM_DataFromWFS(DutchGEOCadastreServerRequest3,Bbox,xPathCadastre2,xPathStringsCadastreTextAngle,-Rdx,-Rdy,1000,1)
+curvesCadastralParcels = GIS2BIM_Lib.GIS2BIM_PointsFromWFS(GIS2BIM_Lib.DutchGEOCadastreServerRequest1,Bbox,GIS2BIM_Lib.xPathCadastre1,-Rdx,-Rdy,1000,3,2)
+curvesBuildings = GIS2BIM_Lib.GIS2BIM_PointsFromWFS(GIS2BIM_Lib.DutchGEOBAG,Bbox,GIS2BIM_Lib.xPathCadastre1,-Rdx,-Rdy,1000,3,2)
+curves3DBAG = GIS2BIM_Lib.GIS2BIM_PointsFromWFS(GIS2BIM_Lib.DutchGEOBAG3D,Bbox,GIS2BIM_Lib.xPath3DBag3,-Rdx,-Rdy,1000,3,3)
 
+
+curvesRuimtelijkePlannen = GIS2BIM_Lib.GIS2BIM_PointsFromWFS(GIS2BIM_Lib.DutchGEORuimtelijkeplannenBouwvlakServerRequest,Bbox,GIS2BIM_Lib.xPathRuimtelijkePlannen,-Rdx,-Rdy,1000,3,2)
+textDataCadastralParcels = GIS2BIM_Lib.GIS2BIM_DataFromWFS(GIS2BIM_Lib.DutchGEOCadastreServerRequest2,Bbox,GIS2BIM_Lib.xPathCadastre2,GIS2BIM_Lib.xPathStringsCadastreTextAngle,-Rdx,-Rdy,1000,3,2)
+textDataOpenbareRuimtenaam = GIS2BIM_Lib.GIS2BIM_DataFromWFS(GIS2BIM_Lib.DutchGEOCadastreServerRequest3,Bbox,GIS2BIM_Lib.xPathCadastre2,GIS2BIM_Lib.xPathStringsCadastreTextAngle,-Rdx,-Rdy,1000,3,2)
+heightData3DBAG = GIS2BIM_Lib.GIS2BIM_DataFromWFS(GIS2BIM_Lib.DutchGEOBAG3D,Bbox,GIS2BIM_Lib.xPath3DBag3,GIS2BIM_Lib.xPathStrings3DBag,-Rdx,-Rdy,1000,3,3)
+
+
+#test = GIS2BIM_Lib.GIS2BIM_PointsFromWFS(GIS2BIM_Lib.DutchGEOBAG3D,Bbox,GIS2BIM_Lib.xPath3DBag3,0,0,1,3,3)
 import Draft
+import Part
+
+for i,j,k in zip(curves3DBAG,heightData3DBAG[1],heightData3DBAG[2]):
+	pointlist = []
+	for curve in i:
+		pointlist.append(FreeCAD.Vector(curve[0], curve[1], float(j)*1000))
+	a = Draft.makeWire(pointlist, closed=True)
+    face = Part.Face(a)
+    solid = face.extrude(Base.Vector(0,0,float(k)*1000))
 
 for i in curvesCadastralParcels:
     pointlist = []
@@ -51,5 +71,17 @@ for i, j, k in zip(textDataOpenbareRuimtenaam[0], textDataOpenbareRuimtenaam[1],
     Text1.ViewObject.FontSize = 1000
     Text1.Placement = Place1
 
+for i,j,k in zip(curves3DBAG,heightData3DBAG[1],heightData3DBAG[2]):
+	pointlist = []
+	for curve in i:
+		pointlist.append(FreeCAD.Vector(curve[0], curve[1], float(j)*1000))
+	extrudeAlong = FreeCAD.Vector(0,0,float(k)*1000)
+	pointlist.append(pointlist[0])
+	a = Part.makePolygon(pointlist)
+	face = Part.Face(a)
+	solid = face.extrude(extrudeAlong)
+	Part.show(solid)
+
 FreeCAD.ActiveDocument.recompute()
 
+'''
--- GIS2BIM_Lib.py
+++ GIS2BIM_Lib.py
@@ -15,17 +15,24 @@ DutchGEOCadastreServerRequest3 = "http://geodata.nationaalgeoregister.nl/kadastr
 DutchGEOBAG = "http://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?service=wfs&version=2.0.0&request=GetFeature&typeName=bag:pand&bbox="
 #Building Contour of BAG
 
+DutchGEOBAG3D = "http://3dbag.bk.tudelft.nl/data/wfs?&request=GetFeature&typeName=BAG3D:pand3d&outputFormat=GML3&bbox="
+#3D Buildings of BAG
+
 DutchGEORuimtelijkeplannenBouwvlakServerRequest = "http://afnemers.ruimtelijkeplannen.nl/afnemers/services?&service=WFS&version=1.1.0&request=GetFeature&typeName=app:Bouwvlak&bbox="
 
 xPathCadastre1 = ".//{http://www.opengis.net/gml/3.2}posList"
 xPathCadastre2 = ".//{http://www.opengis.net/gml/3.2}pos"
 xPathStringsCadastreTextAngle = [".//{http://kadastralekaartv4.geonovum.nl}hoek", ".//{http://kadastralekaartv4.geonovum.nl}tekst"]
 xPathRuimtelijkePlannen = ".//{http://www.opengis.net/gml}posList"
-#Xpath for several Cadastral Servers
+xPathStrings3DBag = [".//{3dbag}ground-0.50", ".//{3dbag}roof-0.50"]
+xPath3DBag3 = ".//{http://www.opengis.net/gml}posList"
+#Xpath for several Web Feature Servers
 
+import urllib.request
+import xml.etree.ElementTree as ET
 import json
 
-def GIS2BIM_GML_poslistData(tree, xPathString,dx,dy,scale,DecimalNumbers):
+def GIS2BIM_GML_poslistData(tree, xPathString,dx,dy,scale,DecimalNumbers,XYZCountDimensions):
 #group X and Y Coordinates of polylines
     posLists = tree.findall(xPathString)
     xyPosList = []
@@ -34,10 +41,10 @@ def GIS2BIM_GML_poslistData(tree, xPathString,dx,dy,scale,DecimalNumbers):
         coordSplit = dataPosList.split()
         x = 0
         coordSplitXY = []
-        for j in range(0, int(len(coordSplit) / 2)):
+        for j in range(0, int(len(coordSplit) / XYZCountDimensions)):
             xy_coord = (round((float(coordSplit[x])+dx)*scale,DecimalNumbers), round((float(coordSplit[x+1])+dy)*scale,DecimalNumbers))
             coordSplitXY.append(xy_coord)
-            x +=2
+            x +=XYZCountDimensions
         xyPosList.append(coordSplitXY)
     return xyPosList
 
@@ -63,22 +70,22 @@ def GIS2BIM_GetLocationDataNetherlands(City,Streetname,Housenumber):
     RD = RD.split()
     RDx = RD[1]
     RDy = RD[2]
-    return RDx,RDy, requesturl
+    return RDx,RDy, requestURL
 
-def GIS2BIM_PointsFromWFS(serverName,boundingBoxString,xPathString,dx,dy,scale,DecimalNumbers):
+def GIS2BIM_PointsFromWFS(serverName,boundingBoxString,xPathString,dx,dy,scale,DecimalNumbers,XYZCountDimensions):
 # group X and Y Coordinates
     myrequesturl = serverName + boundingBoxString
     urlFile = urllib.request.urlopen(myrequesturl)
     tree = ET.parse(urlFile)
-    xyPosList = GIS2BIM_GML_poslistData(tree,xPathString,dx,dy,scale,DecimalNumbers)
+    xyPosList = GIS2BIM_GML_poslistData(tree,xPathString,dx,dy,scale,DecimalNumbers,XYZCountDimensions)
     return xyPosList
 
-def GIS2BIM_DataFromWFS(serverName,boundingBoxString,xPathStringCoord,xPathStrings,dx,dy,scale,DecimalNumbers):
+def GIS2BIM_DataFromWFS(serverName,boundingBoxString,xPathStringCoord,xPathStrings,dx,dy,scale,DecimalNumbers,XYZCountDimensions):
 # group textdata from WFS
     myrequesturl = serverName + boundingBoxString
     urlFile = urllib.request.urlopen(myrequesturl)
     tree = ET.parse(urlFile)
-    xyPosList = GIS2BIM_GML_poslistData(tree,xPathStringCoord,dx,dy,scale,DecimalNumbers)
+    xyPosList = GIS2BIM_GML_poslistData(tree,xPathStringCoord,dx,dy,scale,DecimalNumbers,XYZCountDimensions)
     xPathResults = []
     for xPathString in xPathStrings:
         a = tree.findall(xPathString)
@@ -88,4 +95,3 @@ def GIS2BIM_DataFromWFS(serverName,boundingBoxString,xPathStringCoord,xPathStrin
         xPathResults.append(xPathResulttemp2)
     xPathResults.insert(0,xyPosList)
     return xPathResults
-
