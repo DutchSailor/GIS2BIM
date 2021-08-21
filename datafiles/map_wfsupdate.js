56f2d4d0dd567976b2d5ec55e38332d5da880a0f
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2021-08-21 17:47:29 +0200

    Update map_wfsupdate.js

--- datafiles/map_wfsupdate.js
+++ datafiles/map_wfsupdate.js
@@ -1,1 +1,19 @@
+mymap.removeLayer(wfsGroup)
+var url = 'https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?';
+var params = 'REQUEST&';
+params += 'VERSION&';
+params += 'SERVICE&';
+params += 'TYPENAME&';
+params += 'OUTPUT&';
+params += 'SRS&';
+params += 'BBOX';
+//params += 'bbox=104600,425116,105000,425516';
+var wfsGroup = new L.LayerGroup();
+wfsGroup.addTo(mymap);
 
+$.getJSON(url + params, function(data) {
+	$.each(data.features, function(index, geometry) {
+		wfsCurves = L.geoJson(geometry),
+		wfsGroup.addLayer(wfsCurves)
+	});
+});
